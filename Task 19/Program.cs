9a612de7d31cd6eec6ee9a8071d447d54105480a
Pavel Bodrov <124826427+Indevor03@users.noreply.github.com>
2023-03-18 12:37:16 +0300

    Add files via upload

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. Принимает на вход пятизначное число
+// 2. Проверяет, является ли оно палиндромом
+
+int num, r, sum = 0, t;
+Console.WriteLine("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000)
+{
+    for (t = num; num != 0; num = num / 10)
+    {
+        r = num % 10;
+        sum = sum * 10 + r;
+    }
+    if (t == sum)
+        Console.Write($"{t} является палиндромом");
+    else
+    {
+        Console.Write($"{t} не является палиндромом");
+    }
+}
+else
+{
+    Console.Write("Введите пятизначное число!");
+}
